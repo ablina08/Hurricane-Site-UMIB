7403bffff4865d10ddd2d8d564e0afd20ae04b9c
ablina08 <albinashabanaj12@gmail.com>
2025-01-07 19:40:53 +0100

    Create sf.js

---
+++ Projekti1/sf.js
@@ -0,0 +1,6 @@
+function showImage(index) {
+        const tips = document.querySelectorAll('.tip');
+        // Toggle the active class for the clicked tip
+        tips[index].classList.toggle('active');
+    }
+    
